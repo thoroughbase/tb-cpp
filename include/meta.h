4f023833f12ca7e4e5691b8b2dbb355077a30ec9
dna <dna@thoroughbase.com>
2026-01-21 14:23:10 +0000

    Add tb::either type-checking concept

--- include/meta.h
+++ include/meta.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <concepts>
 #include <type_traits>
 
 namespace tb
@@ -18,5 +19,8 @@ struct type_tag_t {};
 
 template<typename... Ts>
 constexpr type_tag_t<Ts...> type_tag {};
+
+template<typename T, typename... U>
+concept either = (std::same_as<T, U> || ...);
 
 }
